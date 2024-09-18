af6c3076c78dba8dae21471b392cd484c6311b9e
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-09-18 09:23:31 -0400

    creando tabla pregunta_opcion_multiple y pregunta_multiple_criterio_evaluacion

---
+++ database/migrations/2024_09_18_131733_create_pregunta_opcion_multiple_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreguntaOpcionMultipleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pregunta_opcion_multiple', function (Blueprint $table) {
+            $table->id();
+            $table->string('pregunta_opcion_multiple');
+            $table->boolean('estado_opcion_multiple');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pregunta_opcion_multiple');
+    }
+}

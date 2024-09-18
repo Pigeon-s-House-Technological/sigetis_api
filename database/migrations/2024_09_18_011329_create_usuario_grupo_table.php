f56f90fc3540f915f9f5c628b2dac973d30fc7bc
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-09-17 21:19:18 -0400

    feat:database of grupo and grupo_usuario

---
+++ database/migrations/2024_09_18_011329_create_usuario_grupo_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuarioGrupoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario_grupo', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_usuario')->constrained('usuario')->onDelete('cascade');
+            $table->foreignId('id_grupo')->constrained('grupo')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario_grupo');
+    }
+}

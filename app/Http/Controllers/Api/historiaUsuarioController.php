67142e4cc0f328fffd9383ceb82cb7783d31a75e
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-09-21 17:31:09 -0400

    agregando rutas de historia de usuario@

---
+++ app/Http/Controllers/Api/historiaUsuarioController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class historiaUsuarioController extends Controller
+{
+    public function index(){
+        $usuarios = Usuario::all();
+        if($usuarios->isEmpty()){
+            $data = [
+                'message' => 'No hay usuarios registrados',
+                'status' => 200
+            ];
+            return response()->json($data, 404);
+        }
+        return response()->json($usuarios, 200);
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'nombre_user' => 'required',
+            'apellido_user' => 'required',
+            'correo' => 'required|unique:usuario',
+            'tipo_usuario' => 'required'
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en la validacion de los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+        $usuario = Usuario::create([
+            'nombre_user' => $request->nombre_user,
+            'apellido_user' => $request->apellido_user,
+            'correo' => $request->correo,
+            'tipo_usuario' => $request->tipo_usuario
+        ]);
+
+        if(!$usuario){
+            $data = [
+                'message' => 'Error al crear el usuario',
+                'status' => 500
+            ];
+            return response()->json($data, 500);
+        }
+
+        $data = [
+            'message' => $usuario,
+            'status' => 201
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    public function show($id){
+        $usuario = Usuario::find($id);
+        if(!$usuario){
+            $data = [
+                'message' => 'Usuario no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+        $data = [
+            'usuario' => $usuario,
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function destroy($id){
+        $usuario = Usuario::find($id);
+        if(!$usuario){
+            $data = [
+                'message' => 'Usuario no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+        $usuario->delete();
+        $data = [
+            'message' => 'Usuario eliminado',
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function update(Request $request, $id){
+        $usuario = Usuario::find($id);
+        if(!$usuario){
+            $data = [
+                'message' => 'Usuario no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+        $usuario->update([
+            'nombre_user' => $request->nombre_user,
+            'apellido_user' => $request->apellido_user,
+            'correo' => $request->correo,
+            'tipo_usuario' => $request->tipo_usuario
+        ]);
+        $data = [
+            'message' => 'Usuario actualizado',
+            'status' => 200
+        ];
+        return response()->json($data, 200);
+    }
+
+    public function updatePartial(Request $request, $id){
+        $usuario = Usuario::find($id);
+        if(!$usuario){
+            $data = [
+                'message' => 'Usuario no encontrado',
+                'status' => 404
+            ];
+            return response()->json($data, 404);
+        }
+
+        $validator = Validator::make($request->all(), [
+            'nombre_user' => '',
+            'apellido_user' => '',
+            'correo' => 'unique:usuario',
+            'contrasena' => '',
+            'tipo_usuario' => ''
+        ]);
+
+        if($validator->fails()){
+            $data = [
+                'message' => 'Error en la validacion de los datos',
+                'errors' => $validator->errors(),
+                'status' => 400
+            ];
+            return response()->json($data, 400);
+        }
+
+        if($request->has('nombre_user')){
+            $usuario->nombre_user = $request->nombre_user;
+        }
+        if($request->has('apellido_user')){
+            $usuario->apellido_user = $request->apellido_user;
+        }
+        if($request->has('correo')){
+            $usuario->correo = $request->correo;
+        }
+        if($request->has('tipo_usuario')){
+            $usuario->tipo_usuario = $request->tipo_usuario;
+        }
+
+        $usuario->save();
+
+        $data = [
+            'message' => 'Usuario actualizado',
+            'usuario' => $usuario,
+            'status' => 200
+        ];
+
+        return response()->json($data, 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\usuarioController;
+use App\Http\Controllers\Api\historiaUsuarioController;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -16,3 +17,10 @@ Route::post('/usuarios', [usuarioController::class, 'store']);
 Route::put('/usuarios/{id}', [usuarioController::class, 'update']);
 Route::patch('/usuarios/{id}', [usuarioController::class, 'updatePartial']);
 Route::delete('/usuarios/{id}', [usuarioController::class, 'destroy']);
+
+Route::get('/historiaUsuarios', [historiaUsuarioController::class, 'index']);
+Route::get('/historiaUsuarios/{id}', [historiaUsuarioController::class, 'show']);
+Route::post('/historiaUsuarios', [historiaUsuarioController::class, 'store']);
+Route::put('/historiaUsuarios/{id}', [historiaUsuarioController::class, 'update']);
+Route::patch('/historiaUsuarios/{id}', [historiaUsuarioController::class, 'updatePartial']);
+Route::delete('/historiaUsuarios/{id}', [historiaUsuarioController::class, 'destroy']);

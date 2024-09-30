e2e0941b4eb2f9b915d141bc4b983e468734280d
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2024-09-30 00:33:20 -0400

    agregando railwai a database.php

--- config/database.php
+++ config/database.php
@@ -91,31 +91,22 @@ return [
             'prefix_indexes' => true,
         ],
 
+        'railway' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST', 'postgres.railway.internal'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'sigeTIS'),
+            'username' => env('DB_USERNAME', 'postgres'),
+            'password' => env('DB_PASSWORD', 'EZzwHHqdgsuIkmsmceFgZDUXtXgvJBOb'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
+
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Migration Repository Table
-    |--------------------------------------------------------------------------
-    |
-    | This table keeps track of all the migrations that have already run for
-    | your application. Using this information, we can determine which of
-    | the migrations on disk haven't actually been run in the database.
-    |
-    */
-
     'migrations' => 'migrations',
-
-    /*
-    |--------------------------------------------------------------------------
-    | Redis Databases
-    |--------------------------------------------------------------------------
-    |
-    | Redis is an open source, fast, and advanced key-value store that also
-    | provides a richer body of commands than a typical key-value system
-    | such as APC or Memcached. Laravel makes it easy to dig right in.
-    |
-    */
 
     'redis' => [
 
